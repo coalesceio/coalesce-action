7ed6c52144a56fa7f1c4bb26da67c840045e0353
stewartbryson <stewartbryson@gmail.com>
2022-12-09 19:23:02 -0500

    Added GITHUB_WORKSPACE

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
+COA_CONFIG="coa-config"
 
-echo $1 >> COA_CONFIG
-#cat COA_CONFIG
+# For testing locally
+if [[ -z "${GITHUB_WORKSPACE}" ]]; then
+  COA_PATH=${COA_CONFIG}
+else
+  COA_PATH="${GITHUB_WORKSPACE}/${COA_CONFIG}"
+fi
 
-coa --config COA_CONFIG $2
+echo "Writing config file to: '${COA_PATH}'."
+echo $1 >> $COA_PATH
+#cat COA_PATH
+
+coa --config COA_PATH $2
