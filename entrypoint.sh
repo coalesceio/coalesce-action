6ddd01554092197de1440f217e35ab7eb67a5032
stewartbryson <stewartbryson@gmail.com>
2022-12-12 12:27:42 -0500

    Debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,6 @@ COA_CONFIG="/coa-config"
 
 echo "Writing config file to: '${COA_CONFIG}'."
 echo $1 > $COA_CONFIG
-cat "coa config file:\n$COA_CONFIG"
+echo "coa config file:\n$COA_CONFIG"
 
 coa --config $COA_CONFIG $2
