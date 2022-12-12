61fc9663cea371fac8f1ae29948ba37360197bb8
stewartbryson <stewartbryson@gmail.com>
2022-12-12 11:45:17 -0500

    Hard-coded config file path.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,8 @@
 #!/bin/sh
-COA_CONFIG="coa-config"
+COA_CONFIG="/coa-config"
 
-# For testing locally
-if [[ -z "${GITHUB_WORKSPACE}" ]]; then
-  COA_PATH=${COA_CONFIG}
-else
-  COA_PATH="${GITHUB_WORKSPACE}/${COA_CONFIG}"
-fi
+echo "Writing config file to: '${COA_CONFIG}'."
+echo $1 >> $COA_CONFIG
+#cat $COA_CONFIG
 
-echo "Writing config file to: '${COA_PATH}'."
-echo $1 >> $COA_PATH
-#cat COA_PATH
-
-coa --config COA_PATH $2
+coa --config $COA_CONFIG $2
